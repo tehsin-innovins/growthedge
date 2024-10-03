e4f68484d6b13eccf98a961d1f1eb68ff1a5a7bf
Tehsin <tehsin@innovins.com>
2024-10-03 16:49:31 +0530

    Initial commit

--- src/components/FreeEbookModal.js
+++ src/components/FreeEbookModal.js
@@ -6,14 +6,15 @@ import trending from '../assets/home/trending.svg'; // Import the SVG file
 const FreeEbookModal = () => {
 	
 	const [contactus,setContactus] = useState([]);
+	const [pop,setPop] = useState([]);
 	const folderPath = 'https://shareittofriends.com/demo/growthedge/uploads/';    
 	useEffect(() => {
     // Fetch data from API
-    fetch('https://shareittofriends.com/demo/growthedge/blog.php')  // Replace with your actual API URL
+    fetch('https://shareittofriends.com/demo/growthedge/view_popup.php')  // Replace with your actual API URL
       .then((response) => response.json())  // Parse JSON
       .then((data) => {
         setContactus(data.contactdata);  // Store the fetched data
-		
+		setPop(data.popdata);  // Store the fetched data
       })
       .catch((error) => {
         console.error('Error fetching data:', error);
@@ -32,7 +33,7 @@ const FreeEbookModal = () => {
         <div className="modal-content ebookmodal-content">
           <div className="modal-header p-0 pb-2 border-0">
             <h5 className="modal-title">
-              [ Hey Fellas ]
+			{pop.title_1}
             </h5>
 			<a
               type="button"
@@ -42,55 +43,64 @@ const FreeEbookModal = () => {
             ><FontAwesomeIcon icon={faXmark} /></a>
           </div>
           <div className="modal-body p-0">
-						<h4 className="h4">The Ultimate Guide <img src={trending} width={20} height={20} /></h4>
-						<h5 className="h5">60 pages for next 60 days</h5>
-						<h4 className="mt-0 mb-4 h4_">[ THE VIOLET JOURNAL ]</h4>
+						<h4 className="h4">{pop.title_2} <img src={trending} width={20} height={20} /></h4>
+						<h5 className="h5">{pop.title_3}</h5>
+						<h4 className="mt-0 mb-4 h4_">{pop.title_4}</h4>
 
-						<ul class="nav nav-pills nav-justified">
-						<li class="nav-item">
-							<a class="nav-link active" data-bs-toggle="tab" href="#menu1">Summary</a>
+						<ul className="nav nav-pills nav-justified">
+						<li className="nav-item">
+							<a className="nav-link active" data-bs-toggle="tab" href="#menu1">Summary</a>
 						</li>
-						<li class="nav-item">
-							<a class="nav-link" data-bs-toggle="tab" href="#menu2">Index</a>
+						<li className="nav-item">
+							<a className="nav-link" data-bs-toggle="tab" href="#menu2">Index</a>
 						</li>
 						</ul>
 						
 						
-						<div class="tab-content mb-3">
-						<div class="tab-pane container fade" id="menu1">
-							<p className="mt-3 p">Indulge in the assurance of authenticity with our collection of BIS Hallmarked Jewellery, a testament to trust, proudly offered by Kalyan Jewellers. Indulge in the assurance of authenticity with our collection of BIS</p>
+						<div className="tab-content mb-3">
+						<div className="tab-pane container fade" id="menu1">
+							<p className="mt-3 p">{pop.summary_content}</p>
 							
 							<div className="row">
 								<div className="col-lg-4">
 									<div className="row">
 										<div className="col-lg-3 col-md-3">
-											<p className="stitle">12</p>
-											<p className="scontent">Chapters</p>
+											<p className="stitle">{pop.summary_multiple_count_1}</p>
+											<p className="scontent">{pop.summary_multiple_content_1}</p>
 										</div>
 										<div className="col-lg-3 col-md-3">
-											<p className="stitle">12</p>
-											<p className="scontent">Chapters</p>
+											<p className="stitle">{pop.summary_multiple_count_2}</p>
+											<p className="scontent">{pop.summary_multiple_content_2}</p>
 										</div>
 										<div className="col-lg-3 col-md-3">
-											<p className="stitle">12</p>
-											<p className="scontent">Chapters</p>
+											<p className="stitle">{pop.summary_multiple_count_3}</p>
+											<p className="scontent">{pop.summary_multiple_content_3}</p>
 										</div>
 										<div className="col-lg-3 col-md-3">
-											<p className="stitle">12</p>
-											<p className="scontent">Chapters</p>
+											<p className="stitle">{pop.summary_multiple_count_4}</p>
+											<p className="scontent">{pop.summary_multiple_content_4}</p>
 										</div>
 									</div>
 								</div>
 							</div>
 						</div>
-						<div class="tab-pane container fade" id="menu2">
+						<div className="tab-pane container fade" id="menu2">
 							<ul className="index mt-3">
-								<li>Discover</li>
-								<li>Discover</li>
-								<li>Discover</li>
-								<li>Discover</li>
-								<li>Discover</li>
-							</ul>
+  {(() => {
+    // Check if pop.index_multiple_content is defined and is a string
+    if (typeof pop.index_multiple_content === 'string') {
+      // Split the string by commas to create an array of content
+      const index_multiple_content = pop.index_multiple_content.split(',');
+      // Map over the array to create list items
+      return index_multiple_content.map((content2, index) => (
+        <li key={index}>{content2.trim()}</li> // Trim whitespace and add unique key
+      ));
+    } else {
+      return <small></small>; // Fallback when content is undefined
+    }
+  })()}
+</ul>
+
 						</div>
 						
 						</div>
